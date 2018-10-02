2895f45917b170de918f1c421cc67f202d1ee793
Siva Jayaram <sivajrm@gmail.com>
2018-10-02 09:49:23 -0700

    With log4j

--- Library/src/main/java/com/app/library/LibraryApplication.java
+++ Library/src/main/java/com/app/library/LibraryApplication.java
@@ -1,12 +1,16 @@
 package com.app.library;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryApplication {
 
+	private static final Logger logger = LogManager.getLogger(LibraryApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+		logger.debug("--Application Started--");
 	}
 }
