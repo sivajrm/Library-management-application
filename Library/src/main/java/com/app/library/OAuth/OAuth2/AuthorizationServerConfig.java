62af6d9bbfbf137fcf476759f285d9255c0657c0
Siva <sivajrm@gmail.com>
2018-11-18 02:49:50 -0800

    With OAuth2 successfully integrated

--- Library/src/main/java/com/app/library/OAuth/OAuth2/AuthorizationServerConfig.java
+++ Library/src/main/java/com/app/library/OAuth/OAuth2/AuthorizationServerConfig.java
@@ -2,26 +2,16 @@ package com.app.library.OAuth.OAuth2;
 
 import com.app.library.OAuth.User.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
-import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenEndpointFilter;
-import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 import static com.app.library.OAuth.Constants.CLIENT_ID;
-import static com.app.library.OAuth.Constants.RESOURCE_ID;
 
 @Configuration
 @EnableAuthorizationServer
@@ -42,8 +32,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
                 .scopes("read", "write", "trust")
                 .secret("{noop}secret")
-                .accessTokenValiditySeconds(1200)//Access token is only valid for 2 minutes.
-                .refreshTokenValiditySeconds(6000);//Refresh token is only valid for 10 minutes.
+                .accessTokenValiditySeconds(120)//Access token is only valid for 2 minutes.
+                .refreshTokenValiditySeconds(600);//Refresh token is only valid for 10 minutes.
     }
 
     @Override
--- Library/src/main/java/com/app/library/OAuth/Security/WebSecurityConfig.java
+++ Library/src/main/java/com/app/library/OAuth/Security/WebSecurityConfig.java
@@ -7,21 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-//
-//
-import static com.app.library.OAuth.Constants.OAUTH_TOKEN;
 import static com.app.library.OAuth.Constants.SIGN_UP_URL;
-//
-//
+
+
 @Configuration
 @EnableGlobalMethodSecurity()
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,32 +29,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-//
-    @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
-    }
-//
+
     @Autowired
     UserDetailsServiceImpl userDetailsService;
 
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                    .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-//                .anyRequest().authenticated();
-////////                .and()
-////////                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-////////                .addFilter(new JWTAuthorizationFilter(authenticationManager()));
-//    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
+            auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
